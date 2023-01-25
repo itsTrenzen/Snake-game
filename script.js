a2c2c5fb582c867e962c73fe2e344786c31fa6b8
Trenzen <103641280+itsTrenzen@users.noreply.github.com>
2023-01-26 00:24:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,71 @@ const frameHeight = 500;
 
 //snake
 let snakeImg = document.getElementById("imgSnake");
-let moves = ["right"];
+const tileSize = 8;
 let snake = {
         length: 4,
         direction: 'right', 
         posX: 245,
         posY: 245,
         score: 0,
-        queue: ["right"]
-}
-const tileSize = 8;
+        queue: ["right"],
+        tail: [/*{},
+            {id: 2, 
+            x: snake.posX - tileSize*2,
+            y:snake.posY, 
+            getX: function() {return this.x},
+            getY: function() {return this.y},
+            setX: function(p) {this.x += p},
+            setY: function(p) {this.y += p}
+        },
+        {id: 3, 
+            x: snake.posX - tileSize*3,
+            y:snake.posY,
+            getX: function() {return this.x},
+            getY: function() {return this.y},
+            setX: function(p) {this.x += p},
+            setY: function(p) {this.y += p}
+        },
+        {id: 4, 
+            x: snake.posX - tileSize*4,
+            y:snake.posY,
+            getX: function() {return this.x},
+            getY: function() {return this.y},
+            setX: function(p) {this.x += p},
+            setY: function(p) {this.y += p}
+        }*/]
+    }
+    
+snake.tail = [{},
+    {id: 2, 
+    x: snake.posX - tileSize,
+    y: snake.posY, 
+    getX: function() {return this.x},
+    getY: function() {return this.y},
+    setX: function(p) {this.x += p},
+    setY: function(p) {this.y += p}
+},
+{id: 3, 
+    x: snake.posX - tileSize*2,
+    y: snake.posY,
+    getX: function() {return this.x},
+    getY: function() {return this.y},
+    setX: function(p) {this.x += p},
+    setY: function(p) {this.y += p}
+},
+{id: 4, 
+    x: snake.posX - tileSize*3,
+    y: snake.posY,
+    getX: function() {return this.x},
+    getY: function() {return this.y},
+    setX: function(p) {this.x += p},
+    setY: function(p) {this.y += p}
+}];
+
 //apple
 let apple = {
     x: generateApplePosition(),
-    y: generateApplePosition(),//Math.floor(Math.random() * frameHeight) + 8,
+    y: generateApplePosition(),
     imgApple: document.getElementById("imgApple")
 }
 //events
@@ -29,8 +80,59 @@ document.addEventListener('keyup', (e) => {
     else if (e.code == "ArrowLeft" && (snake.direction != "right" && snake.direction != "left")) snake.direction = "left", addDirection("left", snake.length);
     else if (e.code == "ArrowRight" && (snake.direction != "left" && snake.direction != "right")) snake.direction = "right", addDirection("right", snake.length); 
 });
-//running interval
+
 setInterval(() => {
+    ctx.clearRect(0,0, frameWidth, frameHeight);
+
+    for (let i = 0; i < snake.length; i++) { 
+        
+        if (i == 0) {
+           let dir = getDirection();
+            if (dir == "right") {
+                addDirection("right", snake.length);
+                snake.posX += tileSize;
+                ctx.drawImage(snakeImg, snake.posX - i*(tileSize), snake.posY, tileSize, tileSize);
+                }
+            else if (dir == "left") {
+                addDirection("left", snake.length);
+                snake.posX -= tileSize;
+                ctx.drawImage(snakeImg, snake.posX + i*(tileSize), snake.posY, tileSize, tileSize);
+                }
+            else if (dir == "up") {
+                addDirection("up", snake.length);
+                snake.posY -= tileSize; 
+                ctx.drawImage(snakeImg, snake.posX, snake.posY - i*(tileSize), tileSize, tileSize);
+                }
+            else if (dir == "down") {
+                addDirection("down", snake.length);
+                snake.posY += tileSize; 
+                ctx.drawImage(snakeImg, snake.posX, snake.posY + i*(tileSize), tileSize, tileSize);
+                }
+           }
+        else if (i > 0) {
+            let dir = getDirection();
+            if (dir == "right") {
+                snake.tail[i].setX(tileSize);
+                ctx.drawImage(snakeImg, snake.tail[i].getX(), snake.tail[i].getY(), tileSize, tileSize);
+                }
+            else if (dir == "left") {
+                snake.tail[i].setX(-tileSize);
+                ctx.drawImage(snakeImg, snake.tail[i].getX(), snake.tail[i].getY(), tileSize, tileSize);
+                }
+            else if (dir == "up") {
+                snake.tail[i].setY(-tileSize); 
+                ctx.drawImage(snakeImg, snake.tail[i].getX(), snake.tail[i].getY(), tileSize, tileSize);
+                }
+            else if (dir == "down") {
+                snake.tail[i].setY(tileSize); 
+                ctx.drawImage(snakeImg, snake.tail[i].getX(), snake.tail[i].getY(), tileSize, tileSize);
+                }
+        }
+    }
+    ctx.drawImage(apple.imgApple, apple.x, apple.y, tileSize, tileSize);
+}, 200);
+//running interval
+/*setInterval(() => {
     ctx.clearRect(0,0, frameWidth, frameHeight);
 
     for (let i = 0; i < snake.length; i++) { 
@@ -80,6 +182,7 @@ setInterval(() => {
     }
     ctx.drawImage(imgApple, apple.x, apple.y, tileSize, tileSize);
 }, 200);
+*/
 //check for apple
 function isAppleTouched() {
     if (snake.posX == apple.x && snake.posY == apple.y) {
@@ -87,6 +190,15 @@ function isAppleTouched() {
         snake.length += 1;
         document.getElementById("scorePoints").innerHTML = snake.score;
         alert("scored");
+        snake.tail.push({
+            id: snake.length+1, 
+            x: snake.posX - tileSize*(snake.length+1),
+            y:snake.posY,
+            getX: function() {return this.x},
+            getY: function() {return this.y},
+            setX: function(p) {this.x += p},
+            setY: function(p) {this.y += p}
+        });
     }
 }
 //random positon for the apple
